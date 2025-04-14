7d31fd78454a49585d6a32f4a5d1d797e9b8e62f
Shreyafcts <shreya.ghosh@forwardcode.in>
2025-04-14 16:24:31 +0530

    created vacationspot module

--- src/components/Vacationspot.jsx
+++ src/components/Vacationspot.jsx
@@ -1,4 +1,7 @@
 import { hotels } from "../data/hotels";
+import starIcon from '../../public/img/star.svg';
+import mapIcon from '../../public/img/map.svg';
+import costIcon from '../../public/img/cost.svg';
 
 const Vacationspot = () => {
     return (
@@ -10,8 +13,33 @@ const Vacationspot = () => {
                             <h2>Popular vacation spots</h2>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-lg-4"></div>
+                    <div className="row gap-20">
+                        {hotels.map((el, index) => {
+                            return (
+                                <div className="col-lg-4" key={index}>
+                                    <div className="cards__body">
+                                        <div className="_img">
+                                            <img src={el.img} alt="" />
+                                        </div>
+                                        <h4>{el.name}</h4>
+                                        <div className="cards__icon-text">
+                                            <div className="item">
+                                                <img src={starIcon} alt="" />
+                                                <p>{el.rating} <span>({el.totalReviews})</span></p>
+                                            </div>
+                                            <div className="item">
+                                                <img src={costIcon} alt="" />
+                                                <p>{el.cost}</p>
+                                            </div>
+                                            <div className="item">
+                                                <img src={mapIcon} alt="" />
+                                                <p>{el.location}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </section>
